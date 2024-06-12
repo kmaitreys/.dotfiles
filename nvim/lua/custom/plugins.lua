028ddd44ded7b5fe115059a9b2f4a8ab37acc226
kmaitreys <km.maitrey@gmail.com>
2024-06-12 17:27:14 +0530

    copilot?

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -20,6 +20,17 @@ local plugins = {
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end,
+  },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()  -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+  -- The mapping is set to other key, see custom/lua/mappings
+  -- or run <leader>ch to see copilot mapping section
+  end
   }
 }
 return plugins
